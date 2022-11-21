cfad81125332ce4f1ba49c07f3af40b3f4fd7e15
icerittikhai35 <ritthikai.khan@gmai.com>
2022-11-21 18:37:22 +0700

    Update Image Api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
+
+
+type resultProps = { //สร้าง Type มาเพื่อเก็บค่าจาก API Json
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+};
+
 function App() {
+  const [result, setResult] = useState<resultProps[]>([]); // ต้องการ Array จาก resultProps
+
+  useEffect(() => {
+    const api = async () => {
+      const data = await fetch("https://jsonplaceholder.typicode.com/photos", { // Fetch Fn.
+        method: "GET" // GET or POST
+      });
+      const jsonData = await data.json(); //Await JsonData
+      setResult(jsonData); // RQ Object ที่มี Array อยู่ข้างใน
+    };
+    console.table(result[0])
+    api();
+
+  }, []);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {result.map((value, index) => {
+        return (
+          <div className='info' key={index}>
+             <img src={value.thumbnailUrl} alt={value.title} />
+            <h3>{value.title}</h3>
+            <h3>{value.url}</h3>
+          </div>
+        );
+      })}
+
     </div>
   );
+
 }
 
 export default App;
+
+
